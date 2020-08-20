40e769b00058a679afd9b732f5772aad4dc0023d
Aftab Hussain <py-aftabhussain@mobiloitte.com>
2020-08-20 16:17:01 +0530

    Updates

---
+++ precache-manifest.5188c9b05c391980b994131d2d5f3fcc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7439a118794f43c929b79990964ced3c",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "6eda70ec9d8f305186c8",
+    "url": "/reactwebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "63156fdeab5aaeeab2a6",
+    "url": "/reactwebsite/static/css/main.89ef6baa.chunk.css"
+  },
+  {
+    "revision": "6eda70ec9d8f305186c8",
+    "url": "/reactwebsite/static/js/2.a96e0700.chunk.js"
+  },
+  {
+    "revision": "e2392ece3fd3574ed55c718bc4cb6612",
+    "url": "/reactwebsite/static/js/2.a96e0700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63156fdeab5aaeeab2a6",
+    "url": "/reactwebsite/static/js/main.be273d79.chunk.js"
+  },
+  {
+    "revision": "eed1bdfc10bb96feed2a",
+    "url": "/reactwebsite/static/js/runtime-main.a27c2035.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/reactwebsite/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/reactwebsite/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/reactwebsite/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/reactwebsite/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/reactwebsite/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/reactwebsite/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/reactwebsite/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/reactwebsite/static/media/software.43049cae.jpeg"
+  }
+]);
